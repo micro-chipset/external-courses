c1ec20d7389c2711e73d62476913aa8403e12022
chipset <tyrnov.ivan@gmail.com>
2019-03-10 13:30:28 +0300

    Wrote a function that create deep clone object. task-06.js

---
+++ src/ex3_js-objects-part1/task-06.js
@@ -0,0 +1,23 @@
+"use strict"
+
+function deepCopyObject(object) {
+    let clone = {};
+
+    for (let key in object) {
+        if ({}.hasOwnProperty.call(object, key)) {
+            if (typeof(object[key]) === 'object' && object[key] !== null) {
+                if (Array.isArray(object[key])) {
+                    clone[key] = [];
+                    clone[key].push(deepCopyObject(object[key][0]));
+                } else {
+                    clone[key] = deepCopyObject(object[key]);
+                }
+            } else {
+                clone[key] = object[key];
+            }
+        }
+    }
+    return clone;
+}
+
+module.exports = deepCopyObject;
