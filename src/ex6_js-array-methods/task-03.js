8914360651ec4af042d6fe8506e171c515eb8126
chipset <tyrnov.ivan@gmail.com>
2019-03-19 13:44:36 +0300

    Write function everyArray

---
+++ src/ex6_js-array-methods/task-03.js
@@ -0,0 +1,10 @@
+"use strict";
+
+function everyArray(array, callback) {
+  for (let i = 0; i < array.length; i++) {
+    if (!callback(array[i], i, array)) return false;
+  }
+  return true;
+}
+
+module.exports = everyArray;
