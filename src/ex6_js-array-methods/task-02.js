40df0c7932c323f9cbe363550e88ee9a0bfffcef
chipset <tyrnov.ivan@gmail.com>
2019-03-19 12:49:53 +0300

    Write function someArray

---
+++ src/ex6_js-array-methods/task-02.js
@@ -0,0 +1,10 @@
+"use strict";
+
+function someArray(array, callback) {
+  for (let i = 0; i < array.length; i++) {
+    if (callback(array[i], i, array)) return true;
+  }
+  return false;
+}
+
+module.exports = someArray;
