a43a7918865e897dbc6ec0b53dd0982c23642956
chipset <tyrnov.ivan@gmail.com>
2019-05-06 13:20:57 +0300

    Change rating

--- src/library/script.js
+++ src/library/script.js
@@ -47,15 +47,66 @@ function createBook(anotherBook) {
     return bookWrapper;
 }
 
-// Нужно доделать. Пока только вывод 5 звезд.
 function createRating(rating) {
     let stars = document.createElement('div');
     stars.classList.add('book-rating');
     for (let i = 1; i <= 5; i++) {
         let star = document.createElement('i');
-        star.classList.add('fa');
+        star.classList.add('far');
         star.classList.add('fa-star');
         stars.appendChild(star);
     }
+    // Array all stars
+    var allItems = stars.querySelectorAll('.fa-star');
+    // Amount active star
+    var activeItems = stars.querySelectorAll('.fa-star.fas').length;
+    // The function checks where clicked and changes classes
+    var cStars = function (nowPos) {
+        // Remove fas class from all elements.
+        for (var i = 0; allItems.length > i; i++) {
+            allItems[i].classList.remove('fas');
+        }
+        //Add fas class to selected elements
+        for (var i = 0; nowPos + 1 > i; i++) {
+
+            allItems[i].classList.toggle('fas');
+        }
+    }
+    // Hover
+    stars.addEventListener('mouseover', function (e) {
+        var myTarget = e.target;
+        // Array length
+        var i = allItems.length;
+        // Find the selected element in the array and put its index variable
+        while (i--) {
+            if (allItems[i] == myTarget) {
+                var currentIndex = i;
+                break;
+            }
+        }
+        cStars(currentIndex);
+    });
+
+    stars.addEventListener('click', function (e) {
+        // Selected element
+        var myTarget = e.target;
+        // Array length
+        var i = allItems.length;
+        // Find the selected element in the array and put its index variable
+        while (i--) {
+            if (allItems[i] == myTarget) {
+                var currentIndex = i;
+                break;
+            }
+        }
+        cStars(currentIndex);
+        // Change amount active star 
+        activeItems = stars.querySelectorAll('.book-rating .fa-star.fas').length;
+    });
+
+    stars.addEventListener('mouseleave', function (e) {
+        cStars(+activeItems - 1);
+    });
+
     return stars;
 }
