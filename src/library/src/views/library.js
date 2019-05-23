7bab36a1b92973adfc67c43a9b588cb34ff7b009
chipset <tyrnov.ivan@gmail.com>
2019-05-23 18:21:28 +0300

    Add new book

--- src/library/src/views/library.js
+++ src/library/src/views/library.js
@@ -3,9 +3,19 @@ const booksWrapper = document.querySelector('.books');
 const filterTop = document.querySelector(".filter");
 const searchInput = document.querySelector("#search");
 const searchButton = document.querySelector("#search_button");
+const modal = document.querySelector("#modal");
+const modalOverlay = document.querySelector("#modal-overlay");
+const closeButton = document.querySelector("#close-button");
+const openButton = document.querySelector(".button-book-add");
+const form = document.querySelector(".book_add");
+const pushBook = document.querySelector(".submit");
+const errorMessage = document.querySelector(".error_message");
 
 filterTop.addEventListener("click", showFilter);
 searchInput.addEventListener("input", debounce(search, 1000));
+openButton.addEventListener("click", openCloseModal);
+closeButton.addEventListener("click", openCloseModal);
+pushBook.addEventListener("click", validateBook);
 
 // Render books
 function renderBooks(books) {
@@ -114,4 +124,64 @@ function createRating(currentRating) {
     });
 
     return stars;
+}
+
+function openCloseModal() {
+    modal.classList.toggle("closed");
+    modalOverlay.classList.toggle("closed");
+}
+
+function addBook(value) {
+    let book = {};
+    book.id = setId();
+    book.title = setFirstSymbolUpperCase(value.elements.title.value);
+    book.author = {
+        firstName: setFirstSymbolUpperCase(value.elements.firstName.value),
+        lastName: setFirstSymbolUpperCase(value.elements.lastName.value)
+    };
+    book.cost = value.elements.cost.value;
+    book.image_url = value.elements.image_url.value;
+    book.rating = 0;
+    book.categories = Array.from(document.querySelectorAll("input.checkbox:checked")).map(function (elem) {
+        return elem.value;
+    });
+    book.createdAt = new Date().getTime();
+    book.updatedAt = new Date().getTime();
+    books.push(book);
+    booksWrapper.innerHTML = '';
+    renderBooks(books);
+}
+
+function setId() {
+    return +Date.now() + (Math.floor(Math.random() * (999 - 100 + 1)) + 100);
+}
+
+function isNumber(number) {
+    return !isNaN(parseFloat(number)) && isFinite(number);
+}
+
+function setFirstSymbolUpperCase(stringValue) {
+    return stringValue.charAt(0).toUpperCase() + stringValue.slice(1);
+}
+
+function validateBook() {
+    let form = document.forms.book_add;
+    let isValidateData = form.elements.title.value !== ""
+        && form.elements.firstName.value !== ""
+        && form.elements.lastName.value !== ""
+        && form.elements.cost.value !== ""
+        && isNumber(+form.elements.cost.value)
+        && (+form.elements.cost.value) >= 0;
+
+    if (isValidateData) {
+        errorMessage.classList.add("hidden");
+        addBook(form);
+        form.elements.title.value = "";
+        form.elements.firstName.value = "";
+        form.elements.lastName.value = "";
+        form.elements.cost.value = "";
+        openCloseModal();
+    } else {
+        errorMessage.classList.remove("hidden");
+    }
 }
