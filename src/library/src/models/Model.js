4cc3ea9f3bff4c42014ec2d62f6a0ea280ca3c27
micro-chipset <tyrnov.ivan@gmail.com>
2019-05-30 12:34:20 +0300

    Refactoring filter and search

--- src/library/src/models/Model.js
+++ src/library/src/models/Model.js
@@ -2,47 +2,37 @@
     'use strict';
     function Model() {
         this.books = [];
-        this.state = {
-            filteredBooksArray: '',
-            searchBooksArray: '',
-            isFilter: false,
-            isSearch: false,
-        };
+        this.search = '';
     }
 
     Model.prototype.setFilter = function (id) {
         let filterBooks = this.books;
-        if (this.state.isSearch) {
-            filterBooks = this.state.searchBooksArray;
+        if (this.search.length !== 0) {
+            filterBooks = this.calculateSearch();
         }
         switch (id) {
             case 'most_recent':
-                this.state.isFilter = 'most_recent';
-                return this.state.filteredBooksArray = filterBooks.sort((a, b) => b.updatedAt - a.updatedAt);
+                return filterBooks = filterBooks.sort((a, b) => b.updatedAt - a.updatedAt);
             case 'most_popular':
-                this.state.isFilter = 'most_popular';
-                return this.state.filteredBooksArray = filterBooks.sort((a, b) => b.rating - a.rating);
+                return filterBooks = filterBooks.sort((a, b) => b.rating - a.rating);
             case 'free_books':
-                this.state.isFilter = 'free_books';
-                return this.state.filteredBooksArray = filterBooks.filter((item) => item.cost <= 0);
+                return filterBooks = filterBooks.filter((item) => item.cost <= 0);
             default:
-                this.state.isFilter = 'all_books';
-                return this.state.filteredBooksArray = filterBooks.sort((a, b) => a.id - b.id);
+                return filterBooks = filterBooks.sort((a, b) => a.id - b.id);
         }
     }
 
-    Model.prototype.setSearch = function (value) {
-        let searchBooks = this.books;
-        if (this.state.isFilter && value.length !== 0) {
-            searchBooks = this.state.filteredBooksArray;
-        }
-        this.state.isSearch = true;
-        this.state.searchBooksArray = searchBooks.filter((item) =>
-            (item.author.firstName.toLowerCase().indexOf(value) > -1
-                || item.author.lastName.toLowerCase().indexOf(value) > -1
-                || item.title.toLowerCase().indexOf(value) > -1)
+    Model.prototype.calculateSearch = function () {
+        return this.books.filter((item) =>
+            (item.author.firstName.toLowerCase().indexOf(this.search) > -1
+                || item.author.lastName.toLowerCase().indexOf(this.search) > -1
+                || item.title.toLowerCase().indexOf(this.search) > -1)
         );
-        return this.state.searchBooksArray;
+    }
+
+    Model.prototype.setSearch = function (value) {
+        this.search = value;
+        return this.calculateSearch();
     }
 
     window.Model = Model;
