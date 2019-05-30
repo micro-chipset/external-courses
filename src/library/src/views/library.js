8e24eb64e5b841386c8e7603d04cfd8b57e209d5
micro-chipset <tyrnov.ivan@gmail.com>
2019-05-30 09:08:02 +0300

    Methods transferred to the prototype

--- src/library/src/views/library.js
+++ src/library/src/views/library.js
@@ -9,7 +9,6 @@
         const $filterTop = document.querySelector(".filter");
         const $filterTopItem = $filterTop.children;
         const $searchInput = document.querySelector("#search");
-        const $searchButton = document.querySelector("#search_button");
         const $booksWrapper = document.querySelector('.books');
         const $closeButton = document.querySelector("#close-button");
         const $openButton = document.querySelector(".button-book-add");
@@ -98,111 +97,7 @@
             }
         }
 
-        function _createStar   (stars, currentRating) {
-            stars.classList.add('book-rating');
-            for (let i = 1; i <= currentRating; i++) {
-                const $star = document.createElement('i');
-                $star.classList.add('fas');
-                $star.classList.add('fa-star');
-                stars.appendChild($star);
-            }
-            let emptyStars = 5 - currentRating;
-            for (let i = 1; i <= emptyStars; i++) {
-                const $star = document.createElement('i');
-                $star.classList.add('far');
-                $star.classList.add('fa-star');
-                stars.appendChild($star);
-            }
-        }
 
-        function _createRating (currentRating) {
-            let $stars = document.createElement('div');
-            let currentIndex;
-            _createStar($stars, currentRating);
-            // Array all stars
-            let allItems = Array.from($stars.querySelectorAll('.fa-star'));
-            // Amount active star
-            let activeItems = $stars.querySelectorAll('.fa-star.fas').length;
-            // The function checks where clicked and changes classes
-            let cStars = function (nowPos) {
-                // Remove fas and add far class from all elements.
-                for (let i = 0; allItems.length > i; i++) {
-                    allItems[i].classList.remove('fas');
-                    allItems[i].classList.add('far');
-                }
-                //Add fas class to selected elements
-                for (let i = 0; nowPos + 1 > i; i++) {
-
-                    allItems[i].classList.toggle('fas');
-                }
-            }
-            // Hover
-            $stars.addEventListener('mouseover', function (e) {
-                // Selected element
-                let myTarget = e.target;
-                // Find the selected element in the array and put its index variable
-                currentIndex = allItems.findIndex(elem => elem === myTarget);
-                cStars(currentIndex);
-            });
-
-            $stars.addEventListener('click', function (e) {
-                // Selected element
-                let myTarget = e.target;
-                // Find the selected element in the array and put its index variable
-                currentIndex = allItems.findIndex(elem => elem === myTarget);
-                // Reset rating
-                if (currentIndex + 1 === activeItems) {
-                    currentIndex = -1;
-                }
-                cStars(currentIndex);
-                // Change amount active star 
-                activeItems = $stars.querySelectorAll('.book-rating .fa-star.fas').length;
-            });
-
-            $stars.addEventListener('mouseleave', function (e) {
-                cStars(+activeItems - 1);
-            });
-
-            return $stars;
-        }
-
-        this._showButtonDelete = function () {
-            if ($searchInput.value !== "") {
-                $searchButton.firstChild.classList.remove('fa-search')
-                $searchButton.firstChild.classList.add('fa-times')
-            } else {
-                $searchButton.firstChild.classList.remove('fa-times')
-                $searchButton.firstChild.classList.add('fa-search')
-            }
-        }
-
-        this._createBook = function (anotherBook) {
-            const $bookWrapper = document.createElement('article');
-
-            const $bookImg = document.createElement('img');
-            $bookImg.classList.add('book-img')
-            $bookImg.src = anotherBook.image_url;
-            $bookImg.alt = anotherBook.title;
-            $bookWrapper.appendChild($bookImg);
-
-            const $bookTitle = document.createElement('h2');
-            $bookTitle.classList.add('book-title');
-            $bookWrapper.appendChild($bookTitle);
-            let textTitle = document.createTextNode(anotherBook.title);
-            $bookTitle.appendChild(textTitle);
-
-            const $bookAuthor = document.createElement('p');
-            $bookAuthor.classList.add('book-author');
-            $bookWrapper.appendChild($bookAuthor);
-            let textAuthor = document.createTextNode(`by ${anotherBook.author.firstName} ${anotherBook.author.lastName}`);
-            $bookAuthor.appendChild(textAuthor);
-
-            let currentRating = anotherBook.rating;
-            var rating = _createRating(currentRating);
-            $bookWrapper.appendChild(rating);
-
-            return $bookWrapper;
-        }
     }
 
     View.prototype.renderBooks = function (books) {
@@ -219,6 +114,114 @@
             $booksWrapper.innerHTML = "Books not found..."
         }
     }
-    
+
+    View.prototype._createBook = function (anotherBook) {
+        const $bookWrapper = document.createElement('article');
+
+        const $bookImg = document.createElement('img');
+        $bookImg.classList.add('book-img')
+        $bookImg.src = anotherBook.image_url;
+        $bookImg.alt = anotherBook.title;
+        $bookWrapper.appendChild($bookImg);
+
+        const $bookTitle = document.createElement('h2');
+        $bookTitle.classList.add('book-title');
+        $bookWrapper.appendChild($bookTitle);
+        let textTitle = document.createTextNode(anotherBook.title);
+        $bookTitle.appendChild(textTitle);
+
+        const $bookAuthor = document.createElement('p');
+        $bookAuthor.classList.add('book-author');
+        $bookWrapper.appendChild($bookAuthor);
+        let textAuthor = document.createTextNode(`by ${anotherBook.author.firstName} ${anotherBook.author.lastName}`);
+        $bookAuthor.appendChild(textAuthor);
+
+        let currentRating = anotherBook.rating;
+        var rating = this._createRating(currentRating);
+        $bookWrapper.appendChild(rating);
+
+        return $bookWrapper;
+    }
+
+    View.prototype._createRating = function (currentRating) {
+        let $stars = document.createElement('div');
+        let currentIndex;
+        this._createStar($stars, currentRating);
+        // Array all stars
+        let allItems = Array.from($stars.querySelectorAll('.fa-star'));
+        // Amount active star
+        let activeItems = $stars.querySelectorAll('.fa-star.fas').length;
+        // The function checks where clicked and changes classes
+        let cStars = function (nowPos) {
+            // Remove fas and add far class from all elements.
+            for (let i = 0; allItems.length > i; i++) {
+                allItems[i].classList.remove('fas');
+                allItems[i].classList.add('far');
+            }
+            //Add fas class to selected elements
+            for (let i = 0; nowPos + 1 > i; i++) {
+
+                allItems[i].classList.toggle('fas');
+            }
+        }
+        // Hover
+        $stars.addEventListener('mouseover', function (e) {
+            // Selected element
+            let myTarget = e.target;
+            // Find the selected element in the array and put its index variable
+            currentIndex = allItems.findIndex(elem => elem === myTarget);
+            cStars(currentIndex);
+        });
+
+        $stars.addEventListener('click', function (e) {
+            // Selected element
+            let myTarget = e.target;
+            // Find the selected element in the array and put its index variable
+            currentIndex = allItems.findIndex(elem => elem === myTarget);
+            // Reset rating
+            if (currentIndex + 1 === activeItems) {
+                currentIndex = -1;
+            }
+            cStars(currentIndex);
+            // Change amount active star 
+            activeItems = $stars.querySelectorAll('.book-rating .fa-star.fas').length;
+        });
+
+        $stars.addEventListener('mouseleave', function (e) {
+            cStars(+activeItems - 1);
+        });
+
+        return $stars;
+    }
+
+    View.prototype._createStar = function (stars, currentRating) {
+        stars.classList.add('book-rating');
+        for (let i = 1; i <= currentRating; i++) {
+            const $star = document.createElement('i');
+            $star.classList.add('fas');
+            $star.classList.add('fa-star');
+            stars.appendChild($star);
+        }
+        let emptyStars = 5 - currentRating;
+        for (let i = 1; i <= emptyStars; i++) {
+            const $star = document.createElement('i');
+            $star.classList.add('far');
+            $star.classList.add('fa-star');
+            stars.appendChild($star);
+        }
+    }
+
+    View.prototype._showButtonDelete = function () {
+        const $searchInput = document.querySelector("#search");
+        const $searchButton = document.querySelector("#search_button");
+        if ($searchInput.value !== "") {
+            $searchButton.firstChild.classList.remove('fa-search')
+            $searchButton.firstChild.classList.add('fa-times')
+        } else {
+            $searchButton.firstChild.classList.remove('fa-times')
+            $searchButton.firstChild.classList.add('fa-search')
+        }
+    }
+
     window.View = View;
 }());
